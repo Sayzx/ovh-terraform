7932fe19a19836d340fd22de905edea215ca0916
Sayzx <bouclierbleu39@gmail.com>
2026-04-21 13:54:10 +0200

    main: test

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    ovh = {
+      source  = "ovh/ovh"
+      version = "~> 0.40"
+    }
+  }
+}
+
+provider "ovh" {
+  endpoint           = "ovh-eu"
+  application_key    = var.ovh_application_key
+  application_secret = var.ovh_application_secret
+  consumer_key       = var.ovh_consumer_key
+}
+
+resource "ovh_cloud_project_instance" "vm" {
+  service_name = var.project_id
+
+  name       = "aylan-vm-debian"
+  region     = "GRA11"
+  flavor_name = "d2-2"
+
+  image_name = "Debian 12"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ovh_application_key" {}
+variable "ovh_application_secret" {}
+variable "ovh_consumer_key" {}
+variable "project_id" {}
