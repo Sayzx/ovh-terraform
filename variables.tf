54ce4dee59f2198db41c5777a48636d9610a9df0
Sayzx <bouclierbleu39@gmail.com>
2026-04-21 13:58:42 +0200

    fix: tp

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "ovh_application_key" {}
 variable "ovh_application_secret" {}
-variable "ovh_consumer_key" {}
+variable "consumer_key" {}
 variable "project_id" {}
+variable "ssh_key_name" {}
